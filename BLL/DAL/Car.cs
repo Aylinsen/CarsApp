dc3bb8971ae41753f4785a2d35b9f08f78081278
21902484 <21902484@LAB503>
2024-11-20 20:43:08 +0300

    Add project files.

---
+++ BLL/DAL/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BLL.DAL
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+
+      
+        public string Brand { get; set; }
+
+        public bool IsDamaged { get; set; } 
+
+        public DateTime modelYear {  get; set; }    
+
+        public decimal Price { get; set; }
+        public int CarTypeId { get; set; }
+
+        public TypeCar TypeCar { get; set; }  // navigational 
+
+        public List<CarOwner> CarOwners {  get; set; } = new List<CarOwner>();
+
+    }
+}
